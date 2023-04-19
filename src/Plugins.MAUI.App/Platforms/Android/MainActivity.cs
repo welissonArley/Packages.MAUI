309c7bc18c53afa06a4f30b12ac0c70a3c1be813
welissonarley <welissonarleyvs@gmail.com>
2023-04-19 15:01:07 +0100

    fix: can have just portrait mode

--- src/Plugins.MAUI.App/Platforms/Android/MainActivity.cs
+++ src/Plugins.MAUI.App/Platforms/Android/MainActivity.cs
@@ -1,9 +1,11 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace Plugins.MAUI.App;
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+    ScreenOrientation = ScreenOrientation.Portrait)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
