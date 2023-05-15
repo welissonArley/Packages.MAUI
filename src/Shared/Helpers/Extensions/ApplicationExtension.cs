6d95161e48d99626b549031b1a553924536ccb0d
Welisson Arley <welissonarleyvs@gmail.com>
2023-05-15 20:25:38 +0100

    Delete src/Shared directory

--- src/Shared/Helpers/Extensions/ApplicationExtension.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Shared.Helpers.Extensions;
-public static class ApplicationExtension
-{
-    public static bool IsLightMode(this Application application) => application.RequestedTheme == AppTheme.Light;
-    public static Color GetDarkMode(this Application _) => Color.FromArgb("#1F1E19");
-}
