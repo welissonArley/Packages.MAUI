ca2f89c8a8c167ed269555dbeb83a8dca0ec2961
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2024-07-21 19:16:05 -0400

    feat: adding option to add left side button

--- src/PinCodes.Authorization/Views/Components/Keyboards/KeyboardViewer.cs
+++ src/PinCodes.Authorization/Views/Components/Keyboards/KeyboardViewer.cs
@@ -15,11 +15,11 @@ public sealed class KeyboardViewer : ContentPage
         set => SetValue(ShapeViewerProperty, value);
     }
 
-    /*public Button CancelShapeViewer
+    public Button LeftSideButtonShapeViewer
     {
-        get => (Button)GetValue(ShapeViewerProperty);
-        set => SetValue(ShapeViewerProperty, value);
-    }*/
+        get => (Button)GetValue(LeftSideButtonShapeViewerProperty);
+        set => SetValue(LeftSideButtonShapeViewerProperty, value);
+    }
 
     public Button BackspaceViewer
     {
@@ -40,14 +40,16 @@ public sealed class KeyboardViewer : ContentPage
     }
 
     public static readonly BindableProperty ShapeViewerProperty = BindableProperty.Create(nameof(ShapeViewer), typeof(Button), typeof(KeyboardViewer), null, propertyChanged: OnShapePropertyChanged);
-    public static readonly BindableProperty BackspaceViewerProperty = BindableProperty.Create(nameof(BackspaceViewer), typeof(Button), typeof(KeyboardViewer), GetDefaultBackspaceOptionViewShape(), propertyChanged: OnBackspaceViewerPropertyChanged);
-    public static readonly BindableProperty RowSpacingProperty = BindableProperty.Create(nameof(ShapeViewer), typeof(ushort), typeof(KeyboardViewer), SPACING, propertyChanged: OnRowSpacingProperty);
-    public static readonly BindableProperty ColumnSpacingProperty = BindableProperty.Create(nameof(ShapeViewer), typeof(ushort), typeof(KeyboardViewer), SPACING, propertyChanged: OnColumnSpacingProperty);
+    public static readonly BindableProperty LeftSideButtonShapeViewerProperty = BindableProperty.Create(nameof(LeftSideButtonShapeViewer), typeof(Button), typeof(KeyboardViewer), null, propertyChanged: OnLeftSideButtonShapeViewerPropertyChanged);
+    public static readonly BindableProperty BackspaceViewerProperty = BindableProperty.Create(nameof(BackspaceViewer), typeof(Button), typeof(KeyboardViewer), null, propertyChanged: OnBackspaceViewerPropertyChanged);
+    public static readonly BindableProperty RowSpacingProperty = BindableProperty.Create(nameof(RowSpacing), typeof(ushort), typeof(KeyboardViewer), SPACING, propertyChanged: OnRowSpacingPropertyChanged);
+    public static readonly BindableProperty ColumnSpacingProperty = BindableProperty.Create(nameof(ColumnSpacing), typeof(ushort), typeof(KeyboardViewer), SPACING, propertyChanged: OnColumnSpacingPropertyChanged);
 
     private static void OnShapePropertyChanged(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).CreateLayout();
     private static void OnBackspaceViewerPropertyChanged(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).UpdateBackSpaceLayout();
-    private static void OnRowSpacingProperty(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).SetRowColumnSpacing();
-    private static void OnColumnSpacingProperty(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).SetColumnSpacing();
+    private static void OnRowSpacingPropertyChanged(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).SetRowColumnSpacing();
+    private static void OnColumnSpacingPropertyChanged(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).SetColumnSpacing();
+    private static void OnLeftSideButtonShapeViewerPropertyChanged(BindableObject bindable, object oldValue, object newValue) => ((KeyboardViewer)bindable).AddLeftSideButton();
 
     public void SetCommandWhenUserPressButtonOnKeyboard(ICommand callbackCommand) => _callbackKeyboardCommand = callbackCommand;
 
@@ -62,6 +64,14 @@ public sealed class KeyboardViewer : ContentPage
             RowDefinitions = new RowDefinitionCollection(Enumerable.Repeat(new RowDefinition { Height = ShapeViewer.HeightRequest }, 4).ToArray())
         };
 
+        if (LeftSideButtonShapeViewer is not null)
+        {
+            LeftSideButtonShapeViewer.WidthRequest = ShapeViewer.WidthRequest;
+            LeftSideButtonShapeViewer.HeightRequest = ShapeViewer.HeightRequest;
+
+            grid.Add(view: LeftSideButtonShapeViewer, column: 0, row: 3);
+        }
+
         grid.Add(view: AddButtonWithCommand(1), column: 0, row: 0);
         grid.Add(view: AddButtonWithCommand(2), column: 1, row: 0);
         grid.Add(view: AddButtonWithCommand(3), column: 2, row: 0);
@@ -71,9 +81,10 @@ public sealed class KeyboardViewer : ContentPage
         grid.Add(view: AddButtonWithCommand(7), column: 0, row: 2);
         grid.Add(view: AddButtonWithCommand(8), column: 1, row: 2);
         grid.Add(view: AddButtonWithCommand(9), column: 2, row: 2);
-        //_layout.Add(view: CreateCancelOption(), column: 0, row: 3);
         grid.Add(view: AddButtonWithCommand(0), column: 1, row: 3);
-        grid.Add(view: CreateBackspaceOption(), column: 2, row: 3);
+
+        if(BackspaceViewer is not null)
+            grid.Add(view: CreateBackspaceOption(), column: 2, row: 3);
 
         Content = grid;
     }
@@ -83,9 +94,22 @@ public sealed class KeyboardViewer : ContentPage
         if(Content is not null)
         {
             var grid = Content as Grid;
-
-            grid!.RemoveAt(grid.Count);
             grid.Add(view: CreateBackspaceOption(), column: 2, row: 3);
+        }
+    }
+
+    private void AddLeftSideButton()
+    {
+        if (LeftSideButtonShapeViewer is not null && Content is not null)
+        {
+            var grid = Content as Grid;
+
+            LeftSideButtonShapeViewer.WidthRequest = ShapeViewer.WidthRequest;
+            LeftSideButtonShapeViewer.HeightRequest = ShapeViewer.HeightRequest;
+
+            grid!.Insert(0, child: LeftSideButtonShapeViewer);
+            grid.SetColumn(LeftSideButtonShapeViewer, 0);
+            grid.SetRow(LeftSideButtonShapeViewer, 3);
         }
     }
 
@@ -119,22 +143,12 @@ public sealed class KeyboardViewer : ContentPage
     private Button CreateBackspaceOption()
     {
         var button = BackspaceViewer.Clone();
+
+        button.WidthRequest = ShapeViewer.WidthRequest;
+        button.HeightRequest = ShapeViewer.HeightRequest;
+
         button.Command = new Command(() => { _callbackKeyboardCommand?.Execute(-1); });
 
         return button;
-    }
-
-    private static Button GetDefaultBackspaceOptionViewShape()
-    {
-        return new Button
-        {
-            FontSize = 28,
-            FontAttributes = FontAttributes.Bold,
-            WidthRequest = 80,
-            HeightRequest = 80,
-            TextColor = Colors.Black,
-            Text = "⌫",
-            BackgroundColor = Colors.Transparent,
-        };
     }
 }
