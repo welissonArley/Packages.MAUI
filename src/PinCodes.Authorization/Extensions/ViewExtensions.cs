3aef03ff00905afa9785d2cdd46a4ad65cfc594c
Welisson Arley <21105243+welissonArley@users.noreply.github.com>
2025-10-20 18:19:44 -0400

    feat: adding some improvements

--- src/PinCodes.Authorization/Extensions/ViewExtensions.cs
+++ src/PinCodes.Authorization/Extensions/ViewExtensions.cs
@@ -1,20 +1,17 @@
 ﻿using System.Reflection;
 
 namespace PinCodes.Authorization.Extensions;
-public static class ViewExtensions
+internal static class ViewExtensions
 {
-    public static T Clone<T>(this T originalShape) where T : View
+    internal static T Clone<T>(this T originalShape) where T : View
     {
         var shapeType = originalShape.GetType();
         var newShape = (T)Activator.CreateInstance(shapeType)!;
 
-        foreach (var property in shapeType.GetProperties(BindingFlags.Public | BindingFlags.Instance))
+        foreach (var property in shapeType.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(property => property.CanWrite))
         {
-            if (property.CanWrite)
-            {
-                var value = property.GetValue(originalShape);
-                property.SetValue(newShape, value);
-            }
+            var value = property.GetValue(originalShape);
+            property.SetValue(newShape, value);
         }
 
         return newShape;
